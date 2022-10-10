3be5bf79ae9e585d7e53292e0c59356f72d1cb4c
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2022-10-10 15:47:38 +0300

    Done all ex

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,53 @@
-#include <ios>
+#include <iostream>
 using namespace std;
-
 int main()
 {
-	return 0;
+    cout << "\tName       : \"The war and the peace\"" << endl;
+    cout << "\tAvtor      : L.N. Tolstoj" << endl;
+    cout << "\tIzdatelstvo: Piter" << endl;
+    cout << "\tPages      : 500." << endl;
+    cout << "" << endl;
+    cout << "\t\>>Kraj lukomor’a dub zelenyj," << endl;
+    cout << "\tI zolotyj lancyuh na nim:" << endl;
+    cout << "\tShodnya, shonochi kit uchenyj" << endl;
+    cout << "\tNa lancyuhu kruzhl’aje tim\"" << endl;
+    cout << "" << endl;
+    cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)187 << endl;
+    cout << (char)186 << " " << " " << " " << " " << " " << " " << " "
+        << " " << "Pory Roku" << " " << " " << " " << " " << " " << " " << " "
+        << " " << " " << " " << " " << " " << " "
+        << " " << " " << " " << " " << " " << " " << " " << " " << " " << " " << " " << " " << " " << " " << (char)186 << endl;
+    cout << (char)204 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)203 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)203 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)203 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)185 << endl;
+    cout << (char)186 << " " << " " << " " << "Zyma" << " " << " " << " " << (char)186
+        << " " << " " << "Vesna" << " " << " " << " " << (char)186
+        << " " << " " << " " << "Lito" << " " << " " << " " << (char)186
+        << " " << " " << " " << "Osin" << " " << " " << " " << " " << (char)186 << endl;
+    cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)202 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)202 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)202 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+        << (char)188 << endl;
+    return 0;
 }
