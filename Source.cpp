4b959e834b1310ac61bd931026e2131613076d42
De1Mentos <47574489+De1Mentos@users.noreply.github.com>
2022-10-11 17:25:29 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,48 @@
-#include <ios>
+#include <iostream>
 using namespace std;
+double a, b, c, d;
+
 
 int main()
 {
+	cout << "first number" << endl;
+	cin >> a;
+	cout << "second number" << endl;
+	cin >> b;
+	cout << "write number in between" << endl;
+	d = 0;
+	if(b > a)
+	{
+	do
+	{
+		cin >> c;
+		if(c > a && c < b)
+		{
+			cout << "right " << endl;
+			++d;
+		}
+		else
+		{
+			cout << "wrong " << endl;
+		}
+	} while (d == 0);
+	cout << "the end" << endl;
+	}
+	else
+	{
+		do
+		{
+			cin >> c;
+			if (c < a && c > b)
+			{
+				cout << "right " << endl;
+				++d;
+			}
+			else
+			{
+				cout << "wrong " << endl;
+			}
+		} while (d == 0);
+		cout << "the end" << endl;
+	}
 	return 0;
-}
