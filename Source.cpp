9516d0781c065c290d723d8f0f28dd9f23e8c245
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2022-10-13 19:25:02 +0300

    Made EX2

--- Source.cpp
+++ Source.cpp
@@ -6,36 +6,32 @@ using namespace std;
 double col;
 double row;
 char star = '*', space = '_';
-int size;
+int size, number, numbersafe, sumbols = 0, sum = 0, zero = 0;;
 
 int main()
 {
-    int size, a = 1;
-
-    cin >> size;
-
-    for (int i = 0; i < size; ++i)
+    cout << "Number:";
+    cin >> number;
+    numbersafe = number;
+    while (number)
     {
-        for (int j = 0; j < size * 2; ++j)
-        {
-            if (j < size && j < a || j >= size && j >= size * 2 - a)
-                cout << ' ';
-            else
-                cout << '*';
-        }
-
-        if (i < size / 2)
-            a += 2;
-        else
-            a -= 2;
-
-        cout << endl;
+        if (number % 10 == 0)
+            zero++;
+        number = number / 10;
     }
+    cout << "Amount of Zero's: " << zero << endl;
+    for (sumbols = 0; numbersafe > 0; sumbols++)
+    {
+        sum += numbersafe % 10;
+        numbersafe /= 10;
+    }
+    cout << "Amount of numbers = " << sumbols << endl;
+    cout << "Sum = " << sum << endl;
+    cout << "Arithmetic mean = " << (double)sum / n << endl;
 
     system("pause");
     return 0;
 }
-
 //Номер 1{
 //	cout << "h - " << endl;
 //	cin >> col;
@@ -139,6 +135,54 @@ int main()
 //            cout << ' ';
 //        else
 //            cout << '*';
+//    }
+//
+//    if (i < size / 2)
+//        a += 2;
+//    else
+//        a -= 2;
+//
+//    cout << endl;
+//}
+//Номер 6{
+//int size, a = 1;
+//
+//cin >> size;
+//
+//for (int i = 0; i < size; ++i)
+//{
+//    for (int j = 0; j < size * 2; ++j)
+//    {
+//        if (j < size && j < a || j >= size && j >= size * 2 - a)
+//            cout << '*';
+//        else
+//            cout << ' ';
+//    }
+//
+//    if (i < size / 2)
+//        a += 2;
+//    else
+//        a -= 2;
+//
+//    cout << endl;
+//}
+//
+//system("pause");
+//return 0;
+//}
+//Номер 7{
+//int size, a = 1;
+//
+//cin >> size;
+//
+//for (int i = 0; i < size; ++i)
+//{
+//    for (int j = 0; j < size * 2; ++j)
+//    {
+//        if (j < size && j < a)
+//            cout << '*';
+//        else
+//            cout << ' ';
 //    }
 //
 //    if (i < size / 2)
