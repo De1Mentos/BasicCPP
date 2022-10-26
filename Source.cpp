251c32ba324ab3da86a3892d713ea3ed34b5cb18
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2022-10-26 12:05:23 +0300

    Дальше буде краще?

--- Source.cpp
+++ Source.cpp
@@ -7,13 +7,60 @@
 #include <ctime>
 using namespace std;
 
+double Stepen(double x, double y)
+{
+	return pow(x, y);
+}
+double Diapazon(double x, double y, double c, double d)
+{
+	if (y > x)
+	{
+		c = y - x;
+	}
+	else
+	{
+		c = x - y;
+	}
+	const uint32_t size = c;
+	for (size_t i = 0; i < c; i++)
+	{
+		if (y > x)
+		{
+			d += i + x;
+		}
+		else
+		{
+			d += i + y;
+		}
+	}
+	return d + y + x;
+}
+double Dosconale(double x, double y, int c, double d, double a)
+{
+
+	if (y > x)
+	{
+		c = y - x;
+		d = x;
+	}
+	else
+	{
+		c = x - y;
+		d = y;
+	}
+	const uint32_t size = c;
+	for (size_t i = d; i < c; i++)
+	{
+		if (a == 6 || 28 || 496 || 8128 || 33550336)
+		{
+			return a;
+		}
+	}
+}
+
 int main()
 {
-	int Zadacha;
-	int pick;
-	int sumb;
-	int pickdice = 3, psum = 0, rsum = 0;
-	int sum=0;
+	int Zadacha, a=0;
 
 	cout << "\t\t#------------------#" << endl;
 	cout << "\t\t|     THE MENU     |" << endl;
@@ -21,6 +68,11 @@ int main()
 	cout << "\t\t| 1= ZADACHA 1     |" << endl;
 	cout << "\t\t| 2= ZADACHA 2     |" << endl;
 	cout << "\t\t| 3= ZADACHA 3     |" << endl;
+	cout << "\t\t| 4= ZADACHA 4     |" << endl;
+	cout << "\t\t| 5= ZADACHA 5     |" << endl;
+	cout << "\t\t| 6= ZADACHA 6     |" << endl;
+	cout << "\t\t| 7= ZADACHA 7     |" << endl;
+	cout << "\t\t| 8= ZADACHA 8     |" << endl;
 	cout << "\t\t#------------------#" << endl;
 	cout << "\t\t| 0= CLOSE         |" << endl;
 	cout << "\t\t#------------------#" << endl;
@@ -32,112 +84,30 @@ int main()
 	{
 	case 1:
 	{
-		srand(time(0));
-		const uint32_t size = 5;
-		long long arr[size][size];
-		long long sum{};
-		long long mid{};
-		for (int i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-				arr[i][j] = rand() % 1000;
-
-			}
-		}
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < size; j++)
-			{
-				cout << "\t" << arr[i][j];
-
-			}
-			cout << endl;
-		}
-		cout << endl;
-		long long max, min;
-		max = min = arr[0][0];
-		for (size_t i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-			if (min > arr[i][j])
-			{
-				min = arr[i][j];
-			}
-			if (max < arr[i][j])
-			{
-				max = arr[i][j];
-			}
-		    }
-		}
-		for (size_t i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-				sum = sum+arr[i][j];
-			}
-		}
-		for (size_t i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-				sum += arr[i][j];
-			}
-		}
-		for (size_t i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-				mid += arr[i][j];
-			}
-		}
-		mid = mid / pow(size, 2);
-		cout << "Min = " << min << "; Max = " << max << endl;
-		cout << "Sum = " << sum << "; Mid = " << mid << endl;
+		double a, b;
+		cout << "Main number --> ";
+		cin >> a;
+		cout << "\nMain number degree --> ";
+		cin >> b;
+		cout << "\nResult ---> " << Stepen(a, b);
 	}break;
 	case 2:
 	{
-		srand(time(0));
-		const uint32_t size = 4, zesi = 5;
-		long long arr[size][zesi];
-		for (int i = 0; i < size; i++)
-		{
-			for (size_t j = 0; j < size; j++)
-			{
-				arr[i][j] = rand() % 10;
-
-			}
-		}
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < zesi; j++)
-			{
-				arr[0][4] = arr[0][0] + arr[0][1] + arr[0][2] + arr[0][3];
-				arr[1][4] = arr[1][0] + arr[1][1] + arr[1][2] + arr[1][3];
-				arr[2][4] = arr[2][0] + arr[2][1] + arr[2][2] + arr[2][3];
-
-				arr[3][0] = arr[0][0] + arr[1][0] + arr[2][0];
-				arr[3][1] = arr[0][1] + arr[1][1] + arr[2][1];
-				arr[3][2] = arr[0][2] + arr[1][2] + arr[2][2];
-				arr[3][3] = arr[0][3] + arr[1][3] + arr[2][3];
-
-				arr[3][4] = arr[0][4] + arr[1][4] + arr[2][4];
-			}
-			cout << endl;
-		}
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < zesi; j++)
-			{
-				cout << "\t" << arr[i][j];
-			}
-			cout << endl;
-		}
+		double a, b, c=0, d=0;
+		cout << "Main number --> ";
+		cin >> a;
+		cout << "\nSecond Number --> ";
+		cin >> b;
+		cout << "\nResult ---> " << Diapazon(a, b, c, d);
 	}break;
 	case 3:
 	{
-		cout << "no idea";
+		double a, b, c = 0, d = 0;
+		cout << "Main number --> ";
+		cin >> a;
+		cout << "\nSecond Number --> ";
+		cin >> b;
+		cout << "\nResult ---> " << Dosconale(a, b, c, d, a);
 	}break;
-	};
+	}
 }
