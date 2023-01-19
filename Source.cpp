778b7666945c933492e298aa7d1dd825836a7583
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-01-19 21:46:18 +0200

    ЗЩЩ

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,35 @@
-#include <ios>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-
 int main()
 {
-	return 0;
+    fstream file;
+    string text;
+
+    int option;
+    int i = 1;
+    file.open("Source.cpp", ios::in);
+    cin >> option;
+    if (option == 1)
+    {
+        if (file.is_open())
+        {
+            while (getline(file, text))
+            {
+                cout << i << "\t| " << text << endl;
+                i++;
+            }
+        }
+    }
+    else if (option == 2)
+    {
+        if (file.is_open())
+        {
+            cin >> text;
+            file << text << endl;
+            file.close();
+        }
+    }
+    return 0;
 }
