adc5a068bacd93ddbe8df1b21c43c18d40cc2e4a
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-03-30 23:27:48 +0300

    WATER WATER

--- Source.cpp
+++ Source.cpp
@@ -1,51 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
-class Point
-{
-private:
+
+class Reservoir {
 public:
-	int a, b, c, d;
+
+    string name;
+    int width;
+    int lenght;
+    int depth;
+    string type;
+
+    explicit Reservoir(string _name, int _width, int _lenght, int _depth, string _type) {
+        name = _name;
+
+        width = _width;
+        lenght = _lenght;
+        depth = _depth;
+
+        type = _type;
+    }
+
+    inline void Capacity() {
+        cout << "Capacity " << width * lenght * depth;
+    }
+
+    inline void Area() {
+        cout << "Area " << width * lenght;
+    }
+
+    inline void Type() {
+        cout << "Type of reservoir " << type;
+    }
+
 };
 
 int main()
 {
-	int opt = 0;
-	int a;
-	int b;
-	int c;
-	int d;
-	string aut;
-	int s = 0;
-		cout << "Pick the option" << endl;
-		cout << "1 - Set Author name" << endl;
-		cout << "2 - Set rectangle" << endl;
-		cout << "3 - Set square" << endl;
-		cout << "4 - Set rhombus" << endl;
-		cout << "5 - Check stats" << endl;
-		cout << "6 - Leave" << endl;
-		cin >> opt;
-		if (opt == 1)
-		{
-			cout << "Set author name ";
-			cin >> aut;
-		}
-		else if (opt == 2)
-		{
+        Reservoir reservoirOne("RICHKA", 555, 14, 54, "River");
+        reservoirOne.Capacity();
 
-		}
-		else if (opt == 3)
-		{
-
-		}
-		else if (opt == 4)
-		{
-
-		}
-		else if (opt == 5)
-		{
-
-		}
-	return 0;
 }
