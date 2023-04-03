8f00b1853c8505e73adc14bb2387573c132def65
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-03 14:28:50 +0300

    CS GO 2 when?

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,44 @@
-#include <ios>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+
+class Drib 
+{
+public:
+    int a;
+    int b;
+
+    Drib(int _a, int _b) 
+    {
+        a = _a;
+        b = _b;
+    }
+
+    inline void Plus() {
+        cout << a + b << endl;
+    }
+
+    inline void Minus() {
+        cout << a - b << endl;
+    }
+
+    inline void Mnojena() {
+        cout << a * b << endl;
+    }
+
+    inline void Divide() {
+        cout << a / b << endl;
+    }
+
+};
 
 int main()
 {
-	return 0;
+    Drib number(12, 2);
+    number.Plus();
+    number.Minus();
+    number.Divide();
+    number.Mnojena();
 }
