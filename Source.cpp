430470094b3188bfeaf3384d4567cff58cb28d93
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-10 17:45:59 +0300

    Ich bin liebe dich

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,78 @@
-#include <ios>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+class var 
+{
+public:
+    enum Type { INT, DOUBLE, STRING };
+
+    var(int value) 
+    {
+        m_type = INT;
+        m_intValue = value;
+    }
+
+    Type m_type;
+    int m_intValue;
+    double m_doubleValue;
+    string m_stringValue;
+
+    var(double value) 
+    {
+        m_type = DOUBLE;
+        m_doubleValue = value;
+    }
+
+    var(string value) 
+    {
+        m_type = STRING;
+        m_stringValue = value;
+    }
+
+    Type getType() 
+    {
+        return m_type;
+    }
+
+    int getIntValue() 
+    {
+        if (m_type != INT) {
+            throw runtime_error("WARNING - ERROR");
+        }
+        return m_intValue;
+    }
+
+    double getDoubleValue() 
+    {
+        if (m_type != DOUBLE) 
+        {
+            throw runtime_error("WARNING - ERROR");
+        }
+        return m_doubleValue;
+    }
+
+    string getStringValue() 
+    {
+        if (m_type != STRING) 
+        {
+            throw runtime_error("Cannot get string value of non-string var");
+        }
+        return m_stringValue;
+    }
+
+};
 
 int main()
 {
-	return 0;
+        var x(69);
+        var y(6.66);
+        var z("Cheeseburger");
+
+        cout << "x " << x.getIntValue() << endl;
+        cout << "y: " << y.getDoubleValue() << endl;
+        cout << "z: " << z.getStringValue() << endl;
+
+        return 0;
 }
