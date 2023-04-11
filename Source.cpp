e37dd376d94bd79d16c6f28ac93c1e4a2e6b08e1
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-11 22:26:03 +0300

    Save222

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,81 @@
-#include <ios>
-using namespace std;
+#include <iostream>
+
+template<class T>
+class Student
+{
+	T* m_cat;
+public:
+	Student(T* cat = nullptr)
+		:m_cat(cat)
+	{
+	}
+
+	~Student()
+	{
+		delete m_cat;
+	}
+
+	Student(const Student& x)
+	{
+		m_cat = new T;
+		*m_cat = *x.m_cat;
+	}
+
+	Student(Student& x)
+		: m_cat(x.m_cat)
+	{
+		x.m_cat = nullptr; 
+	}
+
+	Student& operator=(const Student&& x)
+	{
+		if (&x == this)
+			return *this;
+
+		delete m_cat;
+
+		m_cat = new T;
+		*m_cat = *x.m_cat;
+
+		return *this;
+	}
+	Student& operator=(Student&& x)
+	{
+		if (&x == this)
+			return *this;
+
+		delete m_cat;
+
+		m_cat = x.m_cat;
+		x.m_cat = nullptr;
+
+		return *this;
+	}
+
+	T& operator*() const { return *m_cat; }
+	T* operator->() const { return m_cat; }
+	bool isNull() const { return m_cat == nullptr; }
+};
+
+class Item
+{
+public:
+	Item() { std::cout << "Item acquired\n"; }
+	~Item() { std::cout << "Item destroyed\n"; }
+};
+
+Student<Item> generateItem()
+{
+	Student<Item> item(new Item);
+	return item; 
+}
+
+
 
 int main()
 {
+	Student<Item> mainItem;
+	mainItem = generateItem();
+
 	return 0;
 }
