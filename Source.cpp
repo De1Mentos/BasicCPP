20065a5459e9184ec5fee3dc5a99e415774c33fa
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-12 16:55:30 +0300

    It's golden yo ma yo

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,94 @@
-#include <ios>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	return 0;
+class CharStack {
+private:
+    const int MAX_SIZE = 100;
+    char stack[100];
+    int top;
+
+public:
+    CharStack() 
+    {
+        top = -1; 
+        for (int i = 0; i < MAX_SIZE; i++) {
+            stack[i] = '\0'; 
+        }
+    }
+
+    bool isEmpty() 
+    { 
+        return top == -1;
+    }
+
+    bool isFull() 
+    {
+        return top == MAX_SIZE - 1;
+    }
+
+    void push(char c) 
+    { 
+        if (!isFull()) {
+            stack[++top] = c;
+        }
+        else {
+            cout << "Stack is full" << endl;
+        }
+    }
+
+    char pop() 
+    {
+        if (!isEmpty()) {
+            return stack[top--];
+        }
+        else {
+            cout << "Stack is empty" << endl;
+            return '\0';
+        }
+    }
+
+    int size() 
+    { 
+        return top + 1;
+    }
+
+    void clear() 
+    {
+        top = -1;
+    }
+
+    char peek()
+        { 
+        if (!isEmpty()) {
+            return stack[top];
+        }
+        else {
+            cout << "Stack is empty" << endl;
+            return '\0';
+        }
+    }
+};
+
+int main() {
+    CharStack stack;
+
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
+
+    cout << "Stack size: " << stack.size() << endl;
+
+    cout << "Top element: " << stack.peek() << endl;
+
+    cout << "Pop: " << stack.pop() << endl;
+
+    cout << "Stack size: " << stack.size() << endl << endl;
+
+    stack.clear();
+
+    cout << "DELETED" << endl << endl;
+
+    cout << "Stack size: " << stack.size() << endl;
+
+    return 0;
 }
