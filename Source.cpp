aaa267c63be105ab2795f951035a32be2dcd6568
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-15 21:53:07 +0300

    something to think about

--- Source.cpp
+++ Source.cpp
@@ -1,44 +1,164 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+
 using namespace std;
 
+template <typename T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
 
-class Drib 
-{
+    Node* top;
+    int size;
+
 public:
-    int a;
-    int b;
+    Stack() : top(nullptr), size(0) {}
 
-    Drib(int _a, int _b) 
-    {
-        a = _a;
-        b = _b;
+    void push(T data) {
+        Node* newNode = new Node;
+        newNode->data = data;
+        newNode->next = top;
+        top = newNode;
+        size++;
     }
 
-    inline void Plus() {
-        cout << a + b << endl;
+    T pop() {
+        if (isEmpty()) {
+            throw runtime_error("Stack is empty!");
+        }
+        Node* temp = top;
+        top = top->next;
+        T data = temp->data;
+        delete temp;
+        size--;
+        return data;
     }
 
-    inline void Minus() {
-        cout << a - b << endl;
+    T peek() {
+        if (isEmpty()) {
+            throw runtime_error("Stack is empty!");
+        }
+        return top->data;
     }
 
-    inline void Mnojena() {
-        cout << a * b << endl;
+    bool isEmpty() {
+        return top == nullptr;
     }
 
-    inline void Divide() {
-        cout << a / b << endl;
+    int getSize() {
+        return size;
     }
-
 };
 
-int main()
-{
-    Drib number(12, 2);
-    number.Plus();
-    number.Minus();
-    number.Divide();
-    number.Mnojena();
+template <typename T>
+class LinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T data, Node* next = nullptr) : data(data), next(next) {}
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+
+    void AddToHead(T data) {
+        head = new Node(data, head);
+        if (tail == nullptr) {
+            tail = head;
+        }
+    }
+
+    void AddToTail(T data) {
+        Node* node = new Node(data);
+        if (head == nullptr) {
+            head = tail = node;
+        }
+        else {
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    void DeleteFromHead() {
+        if (head != nullptr) {
+            Node* node = head;
+            head = head->next;
+            delete node;
+            if (head == nullptr) {
+                tail = nullptr;
+            }
+        }
+    }
+
+    void DeleteFromTail() {
+        if (tail != nullptr) {
+            if (head == tail) {
+                delete head;
+                head = tail = nullptr;
+            }
+            else {
+                Node* node = head;
+                while (node->next != tail) {
+                    node = node->next;
+                }
+                delete tail;
+                tail = node;
+                tail->next = nullptr;
+            }
+        }
+    }
+
+    void DeleteAll() {
+        while (head != nullptr) {
+            Node* node = head;
+            head = head->next;
+            delete node;
+        }
+        tail = nullptr;
+    }
+
+    void Show() const {
+        Node* node = head;
+        while (node != nullptr) {
+            std::cout << node->data << " ";
+            node = node->next;
+        }
+        std::cout << std::endl;
+    }
+
+    ~LinkedList() {
+        DeleteAll();
+    }
+};
+
+int main() {
+    LinkedList<int> list;
+
+    list.AddToHead(1);
+    list.AddToHead(2);
+    list.AddToTail(3);
+    list.AddToTail(4);
+    list.Show();
+    list.DeleteFromHead();
+    list.DeleteFromTail();
+    list.Show();
+    list.DeleteAll();
+    list.Show();
+
+    Stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    cout << "Size: " << stack.getSize() << endl;
+    cout << "Top element: " << stack.peek() << endl;
+    cout << "Popped element: " << stack.pop() << endl;
+    cout << "Popped element: " << stack.pop() << endl;
+    cout << "Size: " << stack.getSize() << endl;
+    return 0;
 }
