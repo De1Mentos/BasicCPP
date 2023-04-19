b024e0a992c715b37f9a0778f221e1139d223a4b
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-19 17:47:35 +0300

    2223

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,36 @@
-#include <ios>
-using namespace std;
+#include <iostream>
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define SQUARE(x) ((x) * (x))
+#define IS_EVEN(x) ((x) % 2 == 0)
+#define IS_ODD(x) ((x) % 2 == 1)
 
-int main()
-{
-	return 0;
+int main() {
+    int a = 5;
+    int b = 7;
+
+    int min_num = MIN(a, b);
+    std::cout << "Min: " << min_num << std::endl;
+
+    int max_num = MAX(a, b);
+    std::cout << "Max: " << max_num << std::endl;
+
+    int square_num = SQUARE(a);
+    std::cout << "Square: " << square_num << std::endl;
+
+    if (IS_EVEN(a)) {
+        std::cout << a << " is even" << std::endl;
+    }
+    else {
+        std::cout << a << " is odd" << std::endl;
+    }
+
+    if (IS_ODD(b)) {
+        std::cout << b << " is odd" << std::endl;
+    }
+    else {
+        std::cout << b << " is even" << std::endl;
+    }
+
+    return 0;
 }
