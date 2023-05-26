9a5368855579d96966177597a29ea98d848fa53a
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-05-26 22:12:03 +0300

    algoriths

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,27 @@
-#include <ios>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-int main()
-{
-	return 0;
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+    bool allGreaterThanZero = std::all_of(numbers.begin(), numbers.end(), [](int num) {
+        return num > 0;
+        });
+    bool anyEqualToThree = std::any_of(numbers.begin(), numbers.end(), [](int num) {
+        return num == 3;
+        });
+    std::vector<int> copyOfNumbers;
+    std::copy(numbers.begin(), numbers.end(), std::back_inserter(copyOfNumbers));
+
+
+    std::cout << "All elements greater than 0: " << std::boolalpha << allGreaterThanZero << std::endl;
+    std::cout << "Any element equal to 3: " << std::boolalpha << anyEqualToThree << std::endl;
+
+    std::cout << "Copy of numbers: ";
+    for (const auto& num : copyOfNumbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
