911cb98b7e4f1f600ca5048236730f3cd38a7e11
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-06-01 17:35:05 +0300

    sussy

--- Source.cpp
+++ Source.cpp
@@ -1,56 +1,62 @@
 #include <iostream>
-using namespace std;
 
 class Date {
 private:
     int day;
     int month;
     int year;
+
 public:
-    Date(int d, int m, int y) {
-        day = d;
-        month = m;
-        year = y;
-    }
-    int operator-(const Date& other) const 
-    {
-        int days = 0;
-        days += (year - other.year) * 365;
-        days += (month - other.month) * 30;
-        days += (day - other.day);
-        return days;
-    }
-    Date operator+(int numDays) const {
-        Date newDate(day, month, year);
-        newDate.day += numDays;
-        while (newDate.day > 30) {
-            newDate.month += 1;
-            newDate.day -= 30;
-            if (newDate.month > 12) {
-                newDate.year += 1;
-                newDate.month = 1;
-            }
-        }
-        return newDate;
-    }
+    Date(int d, int m, int y) : day(d), month(m), year(y) {}
+
     int getDay() const {
         return day;
     }
+
     int getMonth() const {
         return month;
     }
+
     int getYear() const {
         return year;
     }
+
+    int operator-(const Date& other) {
+        int totalDays = 0;
+        totalDays += (year - other.year) * 365;
+        totalDays += (month - other.month) * 30;
+        totalDays += (day - other.day);
+        return totalDays;
+    }
+
+    Date operator+(int days) {
+        int d = day + days;
+        int m = month;
+        int y = year;
+
+        while (d > 30) {
+            d -= 30;
+            m++;
+            if (m > 12) {
+                m = 1;
+                y++;
+            }
+        }
+
+        return Date(d, m, y);
+    }
 };
 
-int main() 
-{
-    Date d1(1, 1, 2020);
-    Date d2(1, 2, 2020);
-    int diff = d2 - d1;
-    cout << "Difference in days: " << diff << endl;
-    Date d3 = d1 + 10;
-    cout << "New date: " << d3.getDay() << "/" << d3.getMonth() << "/" << d3.getYear() << endl;
+int main() {
+    Date date1(10, 5, 2021);
+    Date date2(1, 1, 2021);
+
+    int diff = date1 - date2;
+    std::cout << "Difference in days: " << diff << std::endl;
+
+    Date increasedDate = date1 + 15;
+    std::cout << "Increased date: " << increasedDate.getDay() << "/"
+        << increasedDate.getMonth() << "/" << increasedDate.getYear() << std::endl;
+
     return 0;
 }
